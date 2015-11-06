f593823a8626eb950bfb7adc468b6ad1427f0fc1
ktoddbrown <ktoddbrown@gmail.com>
2015-11-06 15:34:21 -0800

    add aromaticity and restructure code
    chunked the class counts by category
    removed frequency normalization because confusing
    added aromaticity index

--- R/countCompoundTypes.R
+++ R/countCompoundTypes.R
@@ -38,7 +38,9 @@ countCompoundTypes <- function(fileIn='countCompoundTypesInput.csv', fileOut='co
                                massHeader = c('Mass','m.z'),
                                ratioHeaders = c('C', 'H','O', 'N', 'X13C', 'S', 'P', 'C13'),
                                sampleRegStr = '(X.out)|(^X\\d+$)|(std)|(IntCal_)',
-                               maxColReads = 10, as.freq=FALSE, verbose=TRUE){
+                               maxColReads = 10, 
+                               calculateClass=c('compound', 'molecular', 'aromaticity'), 
+                               verbose=TRUE){
   
   header.df <- read.csv(fileIn, nrows=1)
   
@@ -46,9 +48,10 @@ countCompoundTypes <- function(fileIn='countCompoundTypesInput.csv', fileOut='co
   massCols <- as.character(names(header.df) %in% c(massHeader, ratioHeaders))
   massCols[names(header.df) %in% c(massHeader, ratioHeaders)] <- 'numeric'
   massCols[!names(header.df) %in% c(massHeader, ratioHeaders)] <- 'NULL'
-  massChargeRatio <- read.csv(fileIn, colClasses=massCols)
-  massChargeRatio$OtoC <- massChargeRatio$O/massChargeRatio$C
-  massChargeRatio$HtoC <- massChargeRatio$H/massChargeRatio$C
+  massCharacteristic <- read.csv(fileIn, colClasses=massCols)
+  massCharacteristic$OtoC <- massCharacteristic$O/massCharacteristic$C
+  massCharacteristic$HtoC <- massCharacteristic$H/massCharacteristic$C
+  massCharacteristic$AImod <- (1 + massCharacteristic$C - massCharacteristic$O*0.5 - massCharacteristic$S - massCharacteristic$H*0.5)/(massCharacteristic$C - massCharacteristic$O*0.5 - massCharacteristic$S - massCharacteristic$N - massCharacteristic$P)
   
   ###Create the mass counts for each sample
   compounds.df <- data.frame()
@@ -75,84 +78,91 @@ countCompoundTypes <- function(fileIn='countCompoundTypesInput.csv', fileOut='co
     data.df<-data.df[data.df$intensity > 0,]
     
     #combined tables to list m/z for specific samples
-    data.df <- merge(massChargeRatio, data.df)
+    data.df <- merge(massCharacteristic, data.df)
+    
+    categoryCuts <- function(xx, ranges=list(OtoC=c(0,1)), inclusive=list(OtoC=c(TRUE, TRUE)), 
+                             na.rm=TRUE){
+      assert_that(identical(names(ranges), names(inclusive) ))
+      ans = 1
+      for(nameStr in names(ranges)){
+        ans <- ans *((ranges[[nameStr]][1] < xx[[nameStr]]) + 
+                       inclusive[[nameStr]][1]*(ranges[[nameStr]][1]==xx[[nameStr]]))*
+          ((ranges[[nameStr]][2] > xx[[nameStr]]) + 
+             inclusive[[nameStr]][2]*(ranges[[nameStr]][2]==xx[[nameStr]]))
+      }
+      
+      return(sum(ans > 0, na.rm=TRUE))
+    }
     
     #assign counts to each sample based on the ratios associated with each mass
     temp.df <- ddply(data.df, c('sample'), function(xx){
-      categoryCuts <- function(ranges=list(OtoC=c(0,1)), 
-                               inclusive=list(OtoC=c(TRUE, TRUE)), na.rm=TRUE){
-        assert_that(identical(names(ranges), names(inclusive) ))
-        
-        ans = 1
-        
-        for(nameStr in names(ranges)){
-          ans <- ans *((ranges[[nameStr]][1] < xx[[nameStr]]) + 
-                          inclusive[[nameStr]][1]*(ranges[[nameStr]][1]==xx[[nameStr]]))*
-                      ((ranges[[nameStr]][2] > xx[[nameStr]]) + 
-                          inclusive[[nameStr]][2]*(ranges[[nameStr]][2]==xx[[nameStr]]))
-        }
-        
-        return(sum(ans > 0, na.rm=TRUE))
+      ans2 <- data.frame(total_peaks = sum(xx$C >= 0, na.rm = TRUE),
+                         aveOtoC = mean(xx$OtoC, na.rm = TRUE),
+                         aveHtoC = mean(xx$HtoC, na.rm = TRUE))
+      
+      if('molecular' %in% calculateClass){
+        ans2 <- cbind(ans2, data.frame(
+          CHO = sum(xx$C > 0 & xx$N == 0 & xx$S == 0 & xx$P == 0, na.rm = TRUE ), 
+          CHON = sum(xx$C > 0 & xx$N > 0 & xx$S == 0 & xx$P == 0, na.rm = TRUE ),
+          CHOS = sum(xx$C > 0 & xx$N == 0 & xx$S > 0 & xx$P == 0, na.rm = TRUE ),
+          CHOP = sum(xx$C > 0 & xx$N == 0 & xx$S == 0 & xx$P > 0, na.rm = TRUE ),
+          CHONS = sum(xx$C > 0 & xx$N > 0 & xx$S > 0 & xx$P == 0, na.rm = TRUE ),
+          CHONP = sum(xx$C > 0 & xx$N > 0 & xx$S == 0 & xx$P > 0, na.rm = TRUE ),
+          CHOSP = sum(xx$C > 0 & xx$N == 0 & xx$S > 0 & xx$P > 0, na.rm = TRUE ),
+          CHONSP = sum(xx$C > 0 & xx$N > 0 & xx$S > 0 & xx$P > 0, na.rm = TRUE ),
+          Molecular_NA =sum(xx$C == 0 & xx$N == 0 & xx$S == 0 & xx$P == 0, na.rm = TRUE )))
       }
+      if('compound' %in% calculateClass){
+        #November 2015 BOUNDARIES that we have been using: (M Tfaily)
+        #class   O:C(low) O:C(high) H:C(low) H:C(high)
+        #lipid     >0     0.3       1.5        2.5
+        #unsatHC    0     0.125     0.8       <1.5
+        #condHC     0     0.95      0.2       <0.8
+        #protein   >0.3   0.55      1.5        2.3
+        #aminosugar>0.55  0.7       1.5        2.2
+        #carb      >0.7   1.5       1.5        2.5
+        #lignin    >0.125 0.65      0.8       <1.5
+        #tannin    >0.65  1.1       0.8       <1.5
+        ans2 <- cbind(ans2, data.frame(
+          Lipids = categoryCuts(xx, ranges=list(OtoC=c(0, 0.3), HtoC=c(1.5, 2.5)), 
+                                inclusive=list(OtoC=c(FALSE, TRUE), HtoC=c(TRUE,TRUE))),
+          UnSaturated_Hydrocarbons = categoryCuts(xx, ranges=list(OtoC=c(0, 0.125), HtoC=c(0.8, 1.5)), 
+                                                  inclusive=list(OtoC=c(TRUE, TRUE), HtoC=c(TRUE,FALSE))),
+          Condensed_Hydrocarbons   = categoryCuts(xx, ranges=list(OtoC=c(0, 0.95), HtoC=c(0.2, 0.8)), 
+                                                  inclusive=list(OtoC=c(TRUE, TRUE), HtoC=c(TRUE,FALSE))), 
+          Proteins = categoryCuts(xx, ranges=list(OtoC=c(0.3, 0.55), HtoC=c(1.5, 2.3)), 
+                                  inclusive=list(OtoC=c(FALSE, TRUE), HtoC=c(TRUE,TRUE))) ,
+          Amino_Sugars = categoryCuts(xx, ranges=list(OtoC=c(0.55, 0.7), HtoC=c(1.5,2.2)), 
+                                      inclusive=list(OtoC=c(FALSE, TRUE), HtoC=c(TRUE, TRUE))) , 
+          Carbohydrates = categoryCuts(xx, ranges=list(OtoC=c(0.7, 1.5), HtoC=c(1.5, 2.5)), 
+                                       inclusive=list(OtoC=c(FALSE, TRUE), HtoC=c(TRUE,TRUE))) , 
+          Lignin = categoryCuts(xx, ranges=list(OtoC=c(0.125, 0.65), HtoC=c(0.8, 1.5)), 
+                                inclusive=list(OtoC=c(FALSE, TRUE), HtoC=c(TRUE, FALSE))) , 
+          Tannins = categoryCuts(xx, ranges=list(OtoC=c(0.65, 1.1), HtoC=c(0.8, 1.5)), 
+                                 inclusive=list(OtoC=c(FALSE, TRUE), HtoC=c(TRUE, FALSE)))
+        ))
+        ans2$Compounds_NA <- ans2$total_peaks-rowSums(ans2[,c('Lipids', 'UnSaturated_Hydrocarbons', 'Proteins', 'Lignin', 'Carbohydrates', 'Amino_Sugars', 'Tannins', 'Condensed_Hydrocarbons')])
+      }
+      if('aromaticity' %in% calculateClass){
+        ans2 <- cbind(ans2, data.frame(
+          Aliphatics = sum(xx$HtoC >= 1.5 & xx$HtoC < 2.0 & xx$N ==0, na.rm = TRUE),
+          AliphaticsN = sum(xx$HtoC >= 1.5 & xx$HtoC < 2.0 & xx$N > 0, na.rm = TRUE),
+          Saturated = sum(xx$HtoC >= 2.0 | xx$OtoC >= 0.9, na.rm = TRUE), 
+          Condensed_Aromatics = sum(xx$AImod > 0.66, na.rm = TRUE),
+          Aromatic = sum(xx$AImod <= 0.66 & xx$AImod > 0.5, na.rm = TRUE),
+          LigninPhenolics = sum(xx$AImod <= 0.5 & xx$HtoC < 2.0, na.rm = TRUE)
+        ))
+        }
       
-      #NEW BOUNDARIES that we have been using: (M Tfaily)
-      #class   O:C(low) O:C(high) H:C(low) H:C(high)
-      #lipid     >0     0.3       1.5        2.5
-      #unsatHC    0     0.125     0.8       <1.5
-      #condHC     0     0.95      0.2       <0.8
-      #protein   >0.3   0.55      1.5        2.3
-      #aminosugar>0.55  0.7       1.5        2.2
-      #carb      >0.7   1.5       1.5        2.5
-      #lignin    >0.125 0.65      0.8       <1.5
-      #tannin    >0.65  1.1       0.8       <1.5
-      
-      data.frame (Lipids = categoryCuts(ranges=list(OtoC=c(0, 0.3), HtoC=c(1.5, 2.5)), 
-                                     inclusive=list(OtoC=c(FALSE, TRUE), HtoC=c(TRUE,TRUE))),
-        UnSaturated_Hydrocarbons = categoryCuts(ranges=list(OtoC=c(0, 0.125), HtoC=c(0.8, 1.5)), 
-                                                inclusive=list(OtoC=c(TRUE, TRUE), HtoC=c(TRUE,FALSE))),
-        Condensed_Hydrocarbons   = categoryCuts(ranges=list(OtoC=c(0, 0.95), HtoC=c(0.2, 0.8)), 
-                                                inclusive=list(OtoC=c(TRUE, TRUE), HtoC=c(TRUE,FALSE))), 
-        Proteins = categoryCuts(ranges=list(OtoC=c(0.3, 0.55), HtoC=c(1.5, 2.3)), 
-                                inclusive=list(OtoC=c(FALSE, TRUE), HtoC=c(TRUE,TRUE))) ,
-        Amino_Sugars = categoryCuts(ranges=list(OtoC=c(0.55, 0.7), HtoC=c(1.5,2.2)), 
-                                    inclusive=list(OtoC=c(FALSE, TRUE), HtoC=c(TRUE, TRUE))) , 
-        Carbohydrates = categoryCuts(ranges=list(OtoC=c(0.7, 1.5), HtoC=c(1.5, 2.5)), 
-                                     inclusive=list(OtoC=c(FALSE, TRUE), HtoC=c(TRUE,TRUE))) , 
-        Lignin = categoryCuts(ranges=list(OtoC=c(0.125, 0.65), HtoC=c(0.8, 1.5)), 
-                              inclusive=list(OtoC=c(FALSE, TRUE), HtoC=c(TRUE, FALSE))) , 
-        Tannins = categoryCuts(ranges=list(OtoC=c(0.65, 1.1), HtoC=c(0.8, 1.5)), 
-                              inclusive=list(OtoC=c(FALSE, TRUE), HtoC=c(TRUE, FALSE))), 
-        
-        Count = sum(xx$C >= 0, na.rm = TRUE),
-        CHO = sum(xx$C > 0 & xx$N == 0 & xx$S == 0 & xx$P == 0, na.rm = TRUE ), 
-        CHON = sum(xx$C > 0 & xx$N > 0 & xx$S == 0 & xx$P == 0, na.rm = TRUE ),
-        CHOS = sum(xx$C > 0 & xx$N == 0 & xx$S > 0 & xx$P == 0, na.rm = TRUE ),
-        CHOP = sum(xx$C > 0 & xx$N == 0 & xx$S == 0 & xx$P > 0, na.rm = TRUE ),
-        CHONS = sum(xx$C > 0 & xx$N > 0 & xx$S > 0 & xx$P == 0, na.rm = TRUE ),
-        CHONP = sum(xx$C > 0 & xx$N > 0 & xx$S == 0 & xx$P > 0, na.rm = TRUE ),
-        CHOSP = sum(xx$C > 0 & xx$N == 0 & xx$S > 0 & xx$P > 0, na.rm = TRUE ),
-        CHONSP = sum(xx$C > 0 & xx$N > 0 & xx$S > 0 & xx$P > 0, na.rm = TRUE ),
-        Not_Assigned =sum(xx$C == 0 & xx$N == 0 & xx$S == 0 & xx$P == 0, na.rm = TRUE ),
-        aveOtoC = mean(xx$OtoC, na.rm = TRUE),
-        aveHtoC = mean(xx$HtoC, na.rm = TRUE)
-      )
+      return(ans2)
     })
-    #New column called "Other" other is the Count - Sum of all compounds
-    temp.df$Other <- temp.df$Count-rowSums(temp.df[,c('Lipids', 'UnSaturated_Hydrocarbons', 'Proteins', 'Lignin', 'Carbohydrates', 'Amino_Sugars', 'Tannins', 'Condensed_Hydrocarbons')])
     
     compounds.df <- rbind.fill(compounds.df, temp.df)
   }
   
   
-  if(as.freq){
-    compounds.df <- ddply(compounds.df, c('sample', 'Count', 'aveOtoC', 'aveHtoC'), function(xx){
-      return(xx <- xx[!names(xx) %in% c('sample', 'Count', 'aveOtoC', 'aveHtoC')]/xx$Count)
-    })
-  }
-  
-  compounds.df <- compounds.df[,c(1:10, 22, 11:21)]
   write.csv(file=fileOut, compounds.df)
-  #return(list(counts=compounds.df, massInfo=massChargeRatio)) #massChargeRatio is 3.2 Mb
+  #return(list(total_peakss=compounds.df, massInfo=massCharacteristic)) #massCharacteristic is 3.2 Mb
   return(compounds.df)
 }
 
